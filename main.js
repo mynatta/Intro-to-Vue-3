03bc4f70569a300d6d7be361b3f2a2f428416fa4
mynatta <6206021612091@fitm.kmutnb.ac.th>
2021-03-24 10:19:44 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+//const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks'
+            ,desciption : 'The quick brown fox jumps over the lazy dog'
+        }
+    }
+})
